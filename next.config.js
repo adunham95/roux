96c8e34731fe0bb79ed04363627ecc480d7cd46f
Adrian <adunham95@gmail.com>
2023-10-06 11:10:27 -0400

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const runtimeCaching = require('next-pwa/cache');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  pwa: {
+    disable: !isProduction,
+    dest: 'public',
+    runtimeCaching,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,27 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        brand: {
+          '50': '#f2f8f9',
+          '100': '#deedef',
+          '200': '#c1dae0',
+          '300': '#96bfca',
+          '400': '#649dad',
+          '500': '#488192',
+          '600': '#3e6b7c',
+          '700': '#385866',
+          '800': '#344b56',
+          '900': '#2f414a',
+          '950': '#1b2931',
+          DEFAULT: '#649dad',
+        },
+        surface: {
+          DEFAULT: '#e6e6e6',
+        },
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
