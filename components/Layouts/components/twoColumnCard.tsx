149944b8583d60594257c3d041460e3211ef0f4b
Adrian <adunham95@gmail.com>
2023-11-01 22:32:34 -0400

    Added sticky side headers

--- components/Layouts/components/twoColumnCard.tsx
+++ components/Layouts/components/twoColumnCard.tsx
@@ -20,12 +20,14 @@ export const TwoColumnCard = (props: IProps) => {
   return (
     <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
       <div className="px-4 sm:px-0">
-        <h2 className="text-base font-semibold leading-7 text-gray-900">
-          {title}
-        </h2>
-        {subTitle && (
-          <p className="mt-1 text-sm leading-6 text-gray-600">{subTitle}</p>
-        )}
+        <div className="sticky top-0">
+          <h2 className="text-base font-semibold leading-7 text-gray-900 ">
+            {title}
+          </h2>
+          {subTitle && (
+            <p className="mt-1 text-sm leading-6 text-gray-600">{subTitle}</p>
+          )}
+        </div>
       </div>
 
       <form
