a300e5b4034632c2448ffc33ca72257a2b725efe
Adrian <adunham95@gmail.com>
2023-11-02 20:15:15 -0400

    Added beta code requirement

--- graphql/mutations/createUser.ts
+++ graphql/mutations/createUser.ts
@@ -1,3 +1,4 @@
+import betaTokens from '@/db/models/betaTokens';
 import User from '@/db/models/user';
 import Auth from '@/services/auth.service';
 import gql from 'graphql-tag';
@@ -7,6 +8,7 @@ export interface IUserInput {
   lastName: string;
   email: string;
   password: string;
+  betaToken: string;
 }
 
 export const createUserTypeDefs = gql`
@@ -15,17 +17,42 @@ export const createUserTypeDefs = gql`
     lastName: String!
     email: String!
     password: String!
+    betaToken: String
   }
 `;
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function createUser(_: unknown, { input }: { input: IUserInput }) {
-  const password = await Auth.hashPassword(input.password);
-  const email = input.email.trim();
-  const user = new User({ ...input, password, email });
-  user.save();
+  const betaTokenRequired = true;
+  try {
+    console.log('input', input);
+    const currentUsers = await User.find({ email: input.email.trim() });
+    console.log('currentUsers', currentUsers);
+    if (currentUsers.length > 0) {
+      throw new Error('User Already Exists');
+    }
+    if (betaTokenRequired && !input.betaToken) {
+      throw new Error('Beta Token Require');
+    }
+    if (betaTokenRequired && input.betaToken) {
+      const token = await betaTokens.findOne({ token: input.betaToken });
+      console.log({ token: input.betaToken, response: token });
+      if (token.redeemed) {
+        throw new Error('Beta token already redeemed');
+      }
+      token.set({ redeemed: true });
+      token.save();
+    }
+    const password = await Auth.hashPassword(input.password);
+    const email = input.email.trim();
+    const user = new User({ ...input, password, email });
+    user.save({ new: true });
 
-  return user.toJSON();
+    return user.toJSON();
+  } catch (error) {
+    console.log({ error });
+    throw error;
+  }
 }
 
 export default createUser;
