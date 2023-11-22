6a4baeb325cb309f71a5865c3527ada6dd18f075
Adrian <adunham95@gmail.com>
2023-11-21 22:34:29 -0500

    Updated toast element

--- components/toast/toast.tsx
+++ components/toast/toast.tsx
@@ -5,10 +5,42 @@ import {
   ExclamationCircleIcon,
   ExclamationTriangleIcon,
 } from '@heroicons/react/20/solid';
-import { useToast } from '@/stores/toast';
+import { IToast, useToast } from '@/stores/toast';
+import { useEffect } from 'react';
 
 export default function Toasts() {
-  const { toasts, removeToast } = useToast();
+  const { toasts } = useToast();
+
+  return (
+    <>
+      {/* Global notification live region, render this permanently at the end of the document */}
+      <div
+        aria-live="assertive"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+      >
+        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
+          {toasts.map((toast) => (
+            <ToastElement key={toast.id} {...toast} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+function ToastElement(props: IToast) {
+  const { id, style, title, subTitle } = props;
+  const { removeToast } = useToast();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(id);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [id, removeToast]);
 
   function getIcon(
     type: 'danger' | 'success' | 'warning' | 'info' | undefined,
@@ -42,49 +74,30 @@ export default function Toasts() {
   }
 
   return (
-    <>
-      {/* Global notification live region, render this permanently at the end of the document */}
-      <div
-        aria-live="assertive"
-        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
-      >
-        <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
-          {toasts.map((toast) => (
-            <div
-              key={toast.id}
-              className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-surface shadow-lg ring-1 ring-surface-1 ring-opacity-5"
+    <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-surface shadow-lg ring-1 ring-surface-1 ring-opacity-5">
+      <div className="p-4">
+        <div className="flex items-start">
+          <div className="flex-shrink-0">{getIcon(style)}</div>
+          <div className="ml-3 w-0 flex-1 pt-0.5">
+            <p className="text-sm font-medium text-surface-1">{title}</p>
+            {subTitle && (
+              <p className="mt-1 text-sm text-surface-3">{subTitle}</p>
+            )}
+          </div>
+          <div className="ml-4 flex flex-shrink-0">
+            <button
+              type="button"
+              className="inline-flex rounded-md bg-surface text-surface-1 hover:text-surface-2 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
+              onClick={() => {
+                removeToast(id);
+              }}
             >
-              <div className="p-4">
-                <div className="flex items-start">
-                  <div className="flex-shrink-0">{getIcon(toast.style)}</div>
-                  <div className="ml-3 w-0 flex-1 pt-0.5">
-                    <p className="text-sm font-medium text-surface-1">
-                      {toast.title}
-                    </p>
-                    {toast.subTitle && (
-                      <p className="mt-1 text-sm text-surface-3">
-                        {toast.subTitle}
-                      </p>
-                    )}
-                  </div>
-                  <div className="ml-4 flex flex-shrink-0">
-                    <button
-                      type="button"
-                      className="inline-flex rounded-md bg-surface text-surface-1 hover:text-surface-2 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
-                      onClick={() => {
-                        removeToast(toast.id);
-                      }}
-                    >
-                      <span className="sr-only">Close</span>
-                      <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+              <span className="sr-only">Close</span>
+              <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
